f2efd698833c3f8e0d20b728b2c3df50acb69d6d
Talal-94 <mito.gldr9@gmail.com>
2021-11-24 10:43:13 +0300

    ..

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ for (let i = 0; i < myEmojis.length; i++) {
     emojiContainer.append(emoji)
 }
 
+const pushBtn = document.getElementById("push-btn")
+pushBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
+    console.log(emojiInput.value)
+})
